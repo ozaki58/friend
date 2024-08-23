8329782058ea16fee68098503952c7cb2b72b1d1
ozaki <ozaki@7659sw.com>
2024-08-24 06:40:51 +0900

    Integrate backend and frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/App.tsx",
+    "./src/Threads.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
